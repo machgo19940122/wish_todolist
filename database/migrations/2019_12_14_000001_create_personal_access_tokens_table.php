1bbe699bbbd5b1db4c1d4bb4c0553508af186714
machgo19940122 <machgo122@gmail.com>
2022-08-12 15:47:57 +0900

    migration修正

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            Schema::defaultStringLength(191);
+            $table->morphs('notifiable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();

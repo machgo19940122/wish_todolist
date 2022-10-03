cf3e54c4872e1e3aa5becc011e269e994cf026c8
machgo19940122 <machgo122@gmail.com>
2022-10-03 17:04:19 +0900

    sideレスポンシブ

--- resources/views/common/header_side.blade.php
+++ resources/views/common/header_side.blade.php
@@ -35,12 +35,20 @@
 
   <main>  
     <div class="side w-10" id="side_bar">  
-        <ul class="nav nav-stacked">
+        <ul class="nav nav-stacked side_1">
                 <li><a href="/wish/0/folders/0/tasks" >wishlist</a></li>
                 <li ><a href="/todo/0/folders/0/tasks">todolist</a></li>
                 <li><a href="/edit_member/{{session('id')}}">会員情報変更</a></li>
                 <li><a href="/add_friend/{{session('id')}}">お友達設定</a></li>
                 <li><a href="/logout"><form  action="{{ route('logout') }}" method="GET">ログアウト</form></a></li>
+        </ul>
+
+        <ul class="nav nav-stacked side_2">
+                <li><a href="/wish/0/folders/0/tasks" ><i class="fa-solid fa-w"></i></a></li>
+                <li ><a href="/todo/0/folders/0/tasks"><i class="fa-solid fa-t"></i></a></li>
+                <li><a href="/edit_member/{{session('id')}}"><i class="fa-solid fa-user"></i></a></li>
+                <li><a href="/add_friend/{{session('id')}}"><i class="fa-solid fa-user-group"></i></a></li>
+                <li><a href="/logout"><form  action="{{ route('logout') }}" method="GET"><i class="fa-solid fa-right-from-bracket"></i></form></a></li>
         </ul>
     </div>
 
